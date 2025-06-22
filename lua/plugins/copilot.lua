6a248baa21f418659affd3d446853236adb62d05
Sk Shahriar Ahmed Raka <skshahriarahmedraka@gmail.com>
2025-06-22 16:21:51 +0600

    updated: astrovim 0.0.2

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -1,19 +1,28 @@
-
 return {
-  "github/copilot.vim",
-  event = "VeryLazy",
-  config = function()
-    -- Copilot settings
-    -- vim.g.copilot_no_tab_map = true
-    vim.g.copilot_assume_mapped = true
-    vim.g.copilot_tab_fallback = ""
-    
-    -- Custom keymaps
-    local keymap = vim.keymap.set
-    -- keymap("i", "<C-g >", 'copilot#Accept("\\<CR>")', { expr = true, replace_keycodes = false })
-    keymap("i", "<C-j>", 'copilot#Next()', { expr = true, silent = true })
-    keymap("i", "<C-k>", 'copilot#Previous()', { expr = true, silent = true })
-    keymap("i", "<C-o>", 'copilot#Dismiss()', { expr = true, silent = true })
-    keymap("i", "<C-s>", 'copilot#Suggest()', { expr = true, silent = true })
-  end,
+    "github/copilot.vim",
+    event = "VeryLazy",
+    config = function()
+        -- Copilot settings
+        vim.g.copilot_assume_mapped = true
+        vim.g.copilot_tab_fallback = ""
+
+        -- Custom keymaps
+        local keymap = vim.keymap.set
+        keymap("i", "<C-j>", 'copilot#Next()', {
+            expr = true,
+            silent = true
+        })
+        keymap("i", "<C-k>", 'copilot#Previous()', {
+            expr = true,
+            silent = true
+        })
+        keymap("i", "<C-o>", 'copilot#Dismiss()', {
+            expr = true,
+            silent = true
+        })
+        keymap("i", "<C-s>", 'copilot#Suggest()', {
+            expr = true,
+            silent = true
+        })
+    end
 }
